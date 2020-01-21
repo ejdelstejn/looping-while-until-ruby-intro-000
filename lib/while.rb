b89f4deca64022ddc6acf6edf58540cbce8c8b5c
Jessye Ejdelman <jessye.ejdelman@gmail.com>
2020-01-21 02:31:38 +0000

    Automatically backed up by Learn

--- lib/while.rb
+++ lib/while.rb
@@ -1,7 +1,7 @@
 def using_until
   levitation_force = 6
   loop do
-    while levitation_force < 10
+    while levitation_force < 10 do
       puts "Wingardium Leviosa"
       levitation_force += 1
     break
